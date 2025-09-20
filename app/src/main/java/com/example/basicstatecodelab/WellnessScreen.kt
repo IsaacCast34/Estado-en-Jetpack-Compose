336f18379a2b37af23c34e8d524f37b72622a366
Jesus Isaac Barajas Castillo <s21120222@alumnos.itsur.edu.mx>
2025-09-19 23:56:26 -0600

    MutableList aplicado

--- app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -1,12 +1,21 @@
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
+import androidx.compose.ui.Modifier
+import com.example.basicstatecodelab.WellnessTask
 import com.example.basicstatecodelab.WellnessTasksList
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
+    val list = remember { getWellnessTasks().toMutableStateList() }
+
     Column(modifier = modifier) {
         StatefulCounter()
-        WellnessTasksList()
+
+        WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task) }
+        )
     }
 }
+
+fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
--- app/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
@@ -1,21 +1,21 @@
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Checkbox
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 
+// Composable sin estado
 @Composable
 fun WellnessTaskItem(
     taskName: String,
@@ -25,7 +25,8 @@ fun WellnessTaskItem(
     modifier: Modifier = Modifier
 ) {
     Row(
-        modifier = modifier, verticalAlignment = Alignment.CenterVertically
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
             modifier = Modifier
@@ -42,15 +43,17 @@ fun WellnessTaskItem(
         }
     }
 }
+
 @Composable
-fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
+fun WellnessTaskItem(   taskName: String, onClose: () -> Unit, modifier: Modifier = Modifier
+) {
     var checkedState by rememberSaveable { mutableStateOf(false) }
 
     WellnessTaskItem(
         taskName = taskName,
         checked = checkedState,
         onCheckedChange = { newValue -> checkedState = newValue },
-        onClose = {}, // we will implement this later!
+        onClose = onClose,
         modifier = modifier,
     )
 }
